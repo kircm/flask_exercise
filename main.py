066d09c1e05edf9e1abf04446eaf1a1861addbff
kircm <kircmarc@gmail.com>
2021-04-15 00:48:03 +0200

    Fixed bug when considering expiration date on request - moved helper methods below

--- main.py
+++ main.py
@@ -47,20 +47,6 @@ def create_snippet():
         abort(400)
 
 
-def get_snippet_with_name(cur, name):
-    cur.execute("select name, expires_at, snippet from snippets where name = ?", (name,))
-    snippet = cur.fetchone()
-    if snippet:
-        get_snippet_path = url_for('get_snippet', name=name)
-        snippet_obj = {"url": f"{HOST_PORT}/{get_snippet_path}",
-                       "name": f"{snippet[0]}",
-                       "expires_at": f"{snippet[1]}",
-                       "snippet": f"{snippet[2]}"}
-        return snippet_obj
-    else:
-        return None
-
-
 @app.route('/snippets/<name>', methods=['GET'])
 def get_snippet(name=None):
     try:
@@ -68,8 +54,8 @@ def get_snippet(name=None):
         cur = conn.cursor()
         cur.execute("select snippet from snippets where name = ?", (name,))
         snippet = get_snippet_with_name(cur, name)
-        if snippet:
-            now = datetime.now()
+        now = datetime.now()
+        if snippet and not is_expired(snippet, now):
             new_expires_at = now + timedelta(seconds=30)
             cur.execute("UPDATE snippets SET expires_at=? WHERE name = ?", (new_expires_at, name))
             conn.commit()
@@ -82,6 +68,28 @@ def get_snippet(name=None):
         if conn: conn.close()
         print(e)
         abort(404)
+
+
+def is_expired(snippet, now):
+    return get_expires_at(snippet) > now
+
+
+def get_expires_at(snp):
+    return datetime.fromisoformat(snp.get('expires_at', ))
+
+
+def get_snippet_with_name(cur, name):
+    cur.execute("select name, expires_at, snippet from snippets where name = ?", (name,))
+    snippet = cur.fetchone()
+    if snippet:
+        get_snippet_path = url_for('get_snippet', name=name)
+        snippet_obj = {"url": f"{HOST_PORT}/{get_snippet_path}",
+                       "name": f"{snippet[0]}",
+                       "expires_at": f"{snippet[1]}",
+                       "snippet": f"{snippet[2]}"}
+        return snippet_obj
+    else:
+        return None
 
 
 app.run(host='0.0.0.0', port=8080)
